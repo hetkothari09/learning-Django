3486f3116f33abbd35e2aa6904b0fb698f24f2cf
Het Kothari <projectecom0@gmail.com>
2025-01-26 21:55:43 +0530

    connected form with the database

--- Hello/home/models.py
+++ Hello/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phno = models.IntegerField()
+    desc = models.TextField()
+    city = models.CharField(max_length=30)
+    zipcode = models.IntegerField()
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,17 +1,17 @@
 from lib2to3.fixes.fix_input import context
-
 from django.shortcuts import render, HttpResponse
-
+from home.models import Contact
 
 # Create your views here.
 def index(request):
 
-    context = {
-        "variable": "Het Kothari"
-    }
+    # context = {
+    #     "variable": "Het Kothari"
+    # }
     # when we want to return simply a string, then we use HttpResponse
     # return HttpResponse("This is a Homepage")
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+    # return render(request, 'index.html', context)
 
 
 def about(request):
@@ -23,4 +23,15 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phno = request.POST.get("phno")
+        desc = request.POST.get("desc")
+        city = request.POST.get("city")
+        zipcode = request.POST.get("zipcode")
+
+        contact = Contact(name=name, email=email, phno=phno, desc=desc, city=city, zipcode=zipcode)
+        contact.save()
+
     return render(request, 'contact.html')
